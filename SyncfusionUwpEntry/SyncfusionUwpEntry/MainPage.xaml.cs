bb7ac7d91d5bb6d53a75d5a1fa835a968f2f97e4
Nicolai Schönberg <nicolai.schoenberg@futura4retail.com>
2020-02-26 08:47:21 +0100

    Add project files.

---
+++ SyncfusionUwpEntry/SyncfusionUwpEntry/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SyncfusionUwpEntry
+{
+    
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel _vm;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _vm = new MainPageViewModel();
+            BindingContext = _vm;
+        }
+
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _vm.Name = "Hello World";
+        }
+    }
+}
---
+++ SyncfusionUwpEntry/SyncfusionUwpEntry/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SyncfusionUwpEntry
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
